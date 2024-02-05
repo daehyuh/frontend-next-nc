a4b0482308bb22dd15a05a50d47d2612a4c59514
daehy <rkdeown10@naver.com>
2024-02-06 01:40:14 +0900

    feat : change og:image

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "NC 학회",
   description: "한국외대 컴퓨터공학부 NC 학회",
-  image: "logo_black.png",
+  image: "https://hufsnc.com/ogbg.png",
   keywords: ["한국외대", "컴퓨터공학부", "학회", "NC학회"],
   url: "https://hufsnc.com",
 };
