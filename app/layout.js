900f20ff3f788b9e9a924b167b851077059feb49
daehy <rkdeown10@naver.com>
2024-03-09 22:21:15 +0900

    add : change layout

--- app/layout.js
+++ app/layout.js
@@ -4,9 +4,9 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "NC 학회",
+  title: "한국외대 컴퓨터공학부 NC 학회",
   openGraph:{
-    title: "NC 학회",
+    title: "한국외대 컴퓨터공학부 NC 학회",
     description: "한국외대 컴퓨터공학부 NC 학회",
     image: "https://hufsnc.com/ogbg.png",
     keywords: ["한국외대", "컴퓨터공학부", "학회", "NC학회"],
